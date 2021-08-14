1c3a63c76137b3e605d8af0d0df1eeb224376856
nishchay <rathi17nishchay@gmail.com>
2021-08-14 14:54:30 +0530

    templates are added

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+#set FLASK_APP=app.py
+from flask import Flask ,render_template
 
 app= Flask(__name__)
 print(app)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('home.html')
+
+
