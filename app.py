31a7bda19561433a63cc105b85b72ddf6b72b8fa
nishchay <rathi17nishchay@gmail.com>
2021-08-14 19:01:47 +0530

    Inventory Page is added

--- app.py
+++ app.py
@@ -7,7 +7,19 @@ app= Flask(__name__)
 print(app)
 
 @app.route('/')
-def index():
+def home_page():
     return render_template('home.html')
+
+@app.route('/inventory')
+def inventory_page():
+    items = [
+        {'id': 1, 'list_items': 'Step Ladder', 'issue_date': '11-04-2021', 'received_date':'15-04-2021','price':110},
+        {'id': 2, 'list_items': 'Water Container', 'issue_date': '13-04-2021', 'received_date':'14-04-2021','price':20},
+        {'id': 3, 'list_items': 'Mandap', 'issue_date': '10-04-2021', 'received_date':'16-04-2021','price':1000}
+    ]
+    return render_template('inventory.html',items=items)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
